9d52a3bf9c4e42042733aea7ddabfaa42f0329c0
AnjoCabs <billyjoecabatin@gmail.com>
2026-07-04 23:13:38 +0800

    07-HospitalDatabase-SQL-Project.sql

--- hospitaldatabaseproject.sql
+++ hospitaldatabaseproject.sql
@@ -28,14 +28,6 @@ CREATE TABLE appointment
   PRIMARY KEY (`id`)
   );
   
-SET GLOBAL local_infile = 1;
-
-LOAD DATA LOCAL INFILE 'C:/Users/billy/Desktop/DataSets/HospitalDataBase/appoinment1.csv'
-INTO TABLE appointment
-FIELDS TERMINATED BY ','
-LINES TERMINATED BY '\n'
-IGNORE 1 LINES;
-
 CREATE TABLE beds 
 (	`id` INT NOT NULL,
 	`bed_id` varchar(255) DEFAULT NULL,
@@ -47,12 +39,6 @@ CREATE TABLE beds
 	`upload_timestamp` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
 	PRIMARY KEY (`id`)
 );
-
-LOAD DATA LOCAL INFILE 'C:/Users/billy/Desktop/DataSets/HospitalDataBase/beds.csv'
-INTO TABLE beds
-FIELDS TERMINATED BY ','
-LINES TERMINATED BY '\n'
-IGNORE 1 LINES;
 
 CREATE TABLE departments 
 (
@@ -66,12 +52,6 @@ CREATE TABLE departments
   `upload_timestamp` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 );
-
-LOAD DATA LOCAL INFILE 'C:/Users/billy/Desktop/DataSets/HospitalDataBase/department.csv'
-INTO TABLE departments
-FIELDS TERMINATED BY ','
-LINES TERMINATED BY '\n'
-IGNORE 1 LINES;
 
 CREATE TABLE doctors 
 (
@@ -92,12 +72,6 @@ CREATE TABLE doctors
   `upload_timestamp` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 );
-
-LOAD DATA LOCAL INFILE 'C:/Users/billy/Desktop/DataSets/HospitalDataBase/doctor.csv'
-INTO TABLE doctors
-FIELDS TERMINATED BY ','
-LINES TERMINATED BY '\n'
-IGNORE 1 LINES;
 
 CREATE TABLE hospital_bills 
 (
@@ -121,12 +95,6 @@ CREATE TABLE hospital_bills
   PRIMARY KEY (`id`)
 );
 
-LOAD DATA LOCAL INFILE 'C:/Users/billy/Desktop/DataSets/HospitalDataBase/hospital_bills.csv'
-INTO TABLE hospital_bills
-FIELDS TERMINATED BY ','
-LINES TERMINATED BY '\n'
-IGNORE 1 LINES;
-
 CREATE TABLE medical_stock
 (
 	`id` INT NOT NULL,
@@ -145,12 +113,6 @@ CREATE TABLE medical_stock
   PRIMARY KEY (`id`)
 );
 
-LOAD DATA LOCAL INFILE 'C:/Users/billy/Desktop/DataSets/HospitalDataBase/Medical_Stock.csv'
-INTO TABLE medical_stock
-FIELDS TERMINATED BY ','
-LINES TERMINATED BY '\n'
-IGNORE 1 LINES;
-
 CREATE TABLE medical_tests 
 (
 	`id` INT NOT NULL,
@@ -165,12 +127,6 @@ CREATE TABLE medical_tests
   PRIMARY KEY (`id`)
 );
 
-LOAD DATA LOCAL INFILE 'C:/Users/billy/Desktop/DataSets/HospitalDataBase/medical_test.csv'
-INTO TABLE medical_tests
-FIELDS TERMINATED BY ','
-LINES TERMINATED BY '\n'
-IGNORE 1 LINES;
-
 CREATE TABLE medicine_patient
 (
   `id` int NOT NULL,
@@ -181,12 +137,6 @@ CREATE TABLE medicine_patient
   `upload_timestamp` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 );
-
-LOAD DATA LOCAL INFILE 'C:/Users/billy/Desktop/DataSets/HospitalDataBase/medicine_patient.csv'
-INTO TABLE medicine_patient
-FIELDS TERMINATED BY ','
-LINES TERMINATED BY '\n'
-IGNORE 1 LINES;
 
 CREATE TABLE patient 
 (
@@ -209,12 +159,6 @@ CREATE TABLE patient
   PRIMARY KEY (`id`)
 );
 
-LOAD DATA LOCAL INFILE 'C:/Users/billy/Desktop/DataSets/HospitalDataBase/patient.csv'
-INTO TABLE patient
-FIELDS TERMINATED BY ','
-LINES TERMINATED BY '\n'
-IGNORE 1 LINES;
-
 CREATE TABLE patient_tests 
 (
   `id` int NOT NULL,
@@ -234,12 +178,6 @@ CREATE TABLE patient_tests
   PRIMARY KEY (`id`)
 );
 
-LOAD DATA LOCAL INFILE 'C:/Users/billy/Desktop/DataSets/HospitalDataBase/Patient_Tests.csv'
-INTO TABLE patient_tests
-FIELDS TERMINATED BY ','
-LINES TERMINATED BY '\n'
-IGNORE 1 LINES;
-
 CREATE TABLE rooms 
 (
   `id` int NOT NULL,
@@ -255,12 +193,6 @@ CREATE TABLE rooms
   PRIMARY KEY (`id`)
 );
 
-LOAD DATA LOCAL INFILE 'C:/Users/billy/Desktop/DataSets/HospitalDataBase/Rooms.csv'
-INTO TABLE rooms
-FIELDS TERMINATED BY ','
-LINES TERMINATED BY '\n'
-IGNORE 1 LINES;
-
 CREATE TABLE satisfaction_score 
 (
   `id` int NOT NULL,
@@ -274,13 +206,6 @@ CREATE TABLE satisfaction_score
   `upload_timestamp` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 );
-
-
-LOAD DATA LOCAL INFILE 'C:/Users/billy/Desktop/DataSets/HospitalDataBase/satisfaction_score.csv'
-INTO TABLE satisfaction_score
-FIELDS TERMINATED BY ','
-LINES TERMINATED BY '\n'
-IGNORE 1 LINES;
 
 CREATE TABLE staff 
 (
@@ -298,12 +223,6 @@ CREATE TABLE staff
   `upload_timestamp` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 );
-
-LOAD DATA LOCAL INFILE 'C:/Users/billy/Desktop/DataSets/HospitalDataBase/staff.csv'
-INTO TABLE staff
-FIELDS TERMINATED BY ','
-LINES TERMINATED BY '\n'
-IGNORE 1 LINES;
 
 CREATE TABLE supplier 
 (
@@ -324,14 +243,9 @@ CREATE TABLE supplier
   PRIMARY KEY (`id`)
 );
 
-LOAD DATA LOCAL INFILE 'C:/Users/billy/Desktop/DataSets/HospitalDataBase/supplier.csv'
-INTO TABLE supplier
-FIELDS TERMINATED BY ','
-LINES TERMINATED BY '\n'
-IGNORE 1 LINES;
-
 -- PATIENT AND TREATMENT INSIGHTS
 -- 1. How many patients are currently admitted vs. discharged?
+
 SELECT
 	SUM(CASE WHEN
 			status = 'Discharged' THEN 1 END) AS numberDischarged,
@@ -341,6 +255,7 @@ FROM patient;
 
 
 -- 2. What are the most common diagnosis?
+
 SELECT
 	diagnosis,
     COUNT(*) AS count
@@ -350,6 +265,7 @@ ORDER BY count DESC;
 
 
 -- 3. Which patients had the longest hospital stays?
+
 SELECT
 	patient_id,
     name,
@@ -363,6 +279,7 @@ ORDER BY numberDays DESC
 LIMIT 1;
 
 -- 4. How many patients were admitted each month this year?
+
 SELECT
     MONTH(admission_date) AS month_number,
     MONTHNAME(admission_date) AS month_name,
@@ -376,6 +293,7 @@ ORDER BY
     month_number;
     
 -- 5. Which department has the highest patient count?
+
 SELECT
     dp.department_id,
     COUNT(a.patient_id) AS patient_count
@@ -388,6 +306,7 @@ GROUP BY dp.department_id
 ORDER BY patient_count DESC;
 
 -- 6. Which blood group is most common among admitted patients?
+
 SELECT
 	blood_group,
     COUNT(*) AS blood_count
@@ -396,6 +315,7 @@ GROUP BY blood_group
 ORDER BY blood_count DESC;
 
 -- 7. Which doctor treated the most patients?
+
 SELECT
 	a.doctor_id,
     d.name,
@@ -411,6 +331,7 @@ GROUP BY
 ORDER BY patient_count DESC;
 
 -- 8. How many patients have been prescribed medication this month?
+
 SELECT
     COUNT(suggest) AS suggest_medication
 FROM appointment
@@ -418,6 +339,7 @@ WHERE suggest LIKE '%medication%';
 
 -- MEDICAL STOCK AND INVENTORY
 -- 9.Which supplier provides the most types of medicines?
+
 SELECT
 	ms.supplier_id,
     s.name,
@@ -430,11 +352,13 @@ GROUP BY
     s.name;
     
 -- 10. What is the total value of current medical stock?
+
 SELECT
 	SUM(stock_qty * cost_price) AS total_value
 FROM medical_stock;
 
 -- 11. What medicines were gonna be expired in February 2025?
+
 SELECT
 	medicine_id,
     name,
@@ -445,6 +369,7 @@ WHERE
     AND MONTHNAME(expiry_date) = 'February';
     
 -- 12. What is the average cost and markup for each category of medicine?
+
 SELECT
 	name,
     (unit_price - cost_price) AS markupValue
@@ -457,6 +382,7 @@ ORDER BY markupValue DESC;
 
 -- FINANCIAL AND BILLING REPORT
 -- 13. What is the total revenue generated per month?
+
 SELECT
     MONTHNAME(admission_date) AS monthName,
     SUM(paid_amount) AS total_revenue
@@ -465,6 +391,7 @@ WHERE payment_status = 'Paid'
 GROUP BY MONTHNAME(admission_date);
 
 -- 14. Which patients have unpaid bills or partial payments?
+
 SELECT
     hb.patient_id,
     p.name,
@@ -476,6 +403,7 @@ WHERE hb.payment_status = 'Unpaid';
 
 
 -- 15. Which payment method is most commonly used?
+
 SELECT
 	payment_method,
     COUNT(*) AS payment_count
@@ -484,17 +412,20 @@ GROUP BY payment_method
 ORDER BY payment_count DESC;
 
 -- 16. How much was spent on room charges vs. doctor fees?
+
 SELECT
 	SUM(doctor_fees) AS total_doctorfees,
     SUM(room_charges) AS total_roomCharges
 FROM hospital_bills;
 
 -- 17. What is the average hospital bill?
+
 SELECT
 	AVG(paid_amount) AS average_bill
 FROM hospital_bills;
 
 -- 18. Which department generates the most revenue?
+
 SELECT
     dt.department_id,
     dt.name AS department_name,
@@ -514,6 +445,7 @@ ORDER BY dt.department_id,
 
 -- DOCTORS AND STAFF MANAGEMENT
 -- 19. Which doctors are currently available?
+
 SELECT
 	doctor_id,
     name,
@@ -526,6 +458,7 @@ FROM doctors
 WHERE availability = 'Available';
 
 -- 20. What is the average experience (in years) per specialization?
+
 SELECT 
 	specialization,
     ROUND(AVG(experience_years),2) AS average_yearsExperience
@@ -534,6 +467,7 @@ GROUP BY specialization
 ORDER BY average_yearsExperience DESC;
 
 -- 21. Who is the highest paid doctor and in which department?
+
 SELECT
     doctor_id,
     name,
@@ -544,6 +478,7 @@ FROM doctors
 WHERE salary = (SELECT MAX(salary) FROM doctors);
 
 -- 22. What is the staff distribution by role and department?
+
 SELECT
     d.department_id,
     d.name AS department_name,
@@ -555,6 +490,7 @@ GROUP BY d.department_id, d.name, s.role
 ORDER BY d.department_id, s.role;
 
 -- 23. Which doctor has the highest satisfaction rating?
+
 SELECT
     sc.doctor_id,
     d.name,
@@ -573,6 +509,7 @@ HAVING AVG(sc.rating) = (
 
 -- ROOMS AND BEDS UTILIZATION 
 -- 24. Which rooms are currently occupied?
+
 SELECT
 	room_id,
     department_id,
@@ -583,6 +520,7 @@ FROM rooms
 WHERE status = 'Occupied';
 
 -- 25. How many beds are available right now per floor?
+
 SELECT
     floor,
     SUM(CASE WHEN 
@@ -595,6 +533,7 @@ GROUP BY floor
 ORDER BY floor;
 
 -- 26. Which rooms require the most maintenance cost?
+
 SELECT
     room_id,
     room_type,
@@ -607,6 +546,7 @@ WHERE avgmontlymaintenancecost = (
 
 -- TEST AND DIAGNOSTIC TRENDS
 -- 27. Which tests are most frequently performed?
+
 SELECT
 	pt.test_id,
     mt.test_name,
@@ -620,12 +560,14 @@ GROUP BY
 ORDER BY count_test DESC;
 
 -- 28. How many tests required fasting?
+
 SELECT
 	COUNT(*)
 FROM medical_tests
 WHERE fasting_required LIKE '%Yes%';
 
 -- 29. Which doctor prescribed the highest number of tests?
+
 SELECT
 	pt.doctor_id,
     name,
@@ -643,6 +585,7 @@ GROUP BY
 ORDER BY total_count DESC;
 
 -- 30. How much revenue is generated from tests monthly?
+
 SELECT
 	MONTH(test_date) AS monthNum,
     MONTHNAME(test_date) AS month_name,
@@ -654,6 +597,7 @@ GROUP BY
     
 -- PATIENT SATISFACTION INSIGHTS
 -- 31. What is the average satisfaction rating per department?
+	
 SELECT
 	department,
     ROUND(AVG(rating),2) AS average_rating
